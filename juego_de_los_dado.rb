59fefeee083baa801dd40cba76e36b89c418976e
unix-punisher <gerard.dgz@gmail.com>
2013-05-04 15:11:04 -0700

    modificacion de juego de dados

--- juego_de_los_dado.rb
+++ juego_de_los_dado.rb
@@ -1,12 +1,22 @@
 puts "=>Bienvenido"
 puts "=>Hola, como te llamas?"
 nombre = gets.chomp
-puts "=> Hola " + nombre + " vamos a jugar?"
+puts "=> Hola " + nombre + " vamos a jugarcon dos dados?"
+
+dado_1 = rand (6) + 1
+dado_2 = rand (6) + 1
+total = dado_1 + dado_2
+
+resultado_jugador = [ dado_1 , dado_2, total]
+
 # el jugador lanza su propio dado
-a = rand(6) + 1
-puts "=> Hola #{nombre} hiciste #{a} con tu dado!"
+
+puts "=> Hola #{nombre} hiciste #{resultado_jugador[0]} con tu dado!"
+
+=begin
+# la computadora lanza su propio dado
 b = rand(6) + 1
-puts "=> La computadora hizo #{b}"
+puts "=> La computadora hizo #{b}!"
 
 if a > b
 
