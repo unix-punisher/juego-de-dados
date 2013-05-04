e1d4d4b6a5d33a9cfcef11a3d551e1906d181dfb
unix-punisher <gerard.dgz@gmail.com>
2013-05-04 12:27:58 -0700

    primer mensaje con git

--- juego_de_los_dado.rb
+++ juego_de_los_dado.rb
@@ -1,1 +1,10 @@
-puts "=>Bienvenido "
+puts "=>Bienvenido"
+puts "=>Hola, como te llamas?"
+nombre = gets.chomp
+puts "=> Hola " + nombre + " vamos a jugar?"
+# el jugador lanza su propio dado
+a = rand(6) + 1
+puts "=> Hola #{nombre} hiciste #{a} con tu dado!"
+b = rand(6) + 1
+puts "=> La computadora hizo #{b}"
+
