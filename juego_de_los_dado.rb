52c652305c3a18c8a7f1239697c67738e08a45c4
unix-punisher <gerard.dgz@gmail.com>
2013-05-04 15:43:01 -0700

    mas cambios

--- juego_de_los_dado.rb
+++ juego_de_los_dado.rb
@@ -1,17 +1,28 @@
 puts "=>Bienvenido"
 puts "=>Hola, como te llamas?"
 nombre = gets.chomp
-puts "=> Hola " + nombre + " vamos a jugarcon dos dados?"
+puts "=> Hola " + nombre + " vamos a jugar con los dados?"
 
-dado_1 = rand (6) + 1
-dado_2 = rand (6) + 1
+dado_1 = rand(6) + 1
+dado_2 = rand(6) + 1
 total = dado_1 + dado_2
 
 resultado_jugador = [ dado_1 , dado_2, total]
 
 # el jugador lanza su propio dado
 
-puts "=> Hola #{nombre} hiciste #{resultado_jugador[0]} con tu dado!"
+puts "=> Hola #{nombre} el primer dado tiene #{resultado_jugador[0]} el segundo #{resultado_jugador[1]} y el total es #{resultado_jugador[2]}!"
+
+dado_11 = rand(6) + 1
+dado_22 = rand(6) + 1
+total = dado_11 + dado_12
+
+resultado_jugador = [ dado_11 , dado_12, total12]
+
+# el jugador lanza su propio dado
+
+puts "=> Hola en mi primer dado tengo #{resultado_jugador[0]} el segundo #{resultado_jugador[1]} y el total es #{resultado_jugador[2]}!"
+
 
 =begin
 # la computadora lanza su propio dado
@@ -33,3 +44,6 @@ else
   puts "=> Empate! Amigos como antes!"
 
 end
+
+
+=end
